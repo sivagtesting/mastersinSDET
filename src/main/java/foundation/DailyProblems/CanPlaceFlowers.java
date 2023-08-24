41901f7cf8bf6c7351e8d97e013b0f5668f2e515
SivaG <47685173+sivagtesting@users.noreply.github.com>
2023-08-24 08:34:57 +0530

    DailyExercise - 21-Aug-2023

---
+++ src/main/java/foundation/DailyProblems/CanPlaceFlowers.java
@@ -0,0 +1,61 @@
+package foundation.DailyProblems;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+public class CanPlaceFlowers {
+
+	public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+		if (flowerbed.length == 1) {
+			if (flowerbed[0] == 0) {
+				flowerbed[0] = 1;
+				n--;
+			}
+		} else {
+			for (int i = 1; i < flowerbed.length; i++) {
+				if (i == 0) {
+					if (flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+						flowerbed[i] = 1;
+						n--;
+					}
+				} else if (i == flowerbed.length - 1) {
+					if (flowerbed[i] == 0 && flowerbed[i - 1] == 0) {
+						flowerbed[i] = 1;
+						n--;
+					}
+				} else {
+					if (flowerbed[i] == 0) {
+						if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0) {
+							n--;
+							flowerbed[i] = 1;
+						}
+
+					}
+				}
+			}
+		}
+
+		return n <= 0;
+	}
+
+	@Test
+	public void test1() {
+		Assert.assertEquals(true, canPlaceFlowers(new int[] { 1, 0, 0, 0, 1 }, 1));
+	}
+
+	@Test
+	public void test2() {
+		Assert.assertEquals(true, canPlaceFlowers(new int[] { 0 }, 1));
+	}
+
+	@Test
+	public void test3() {
+		Assert.assertEquals(false, canPlaceFlowers(new int[] { 1 }, 1));
+	}
+
+	@Test
+	public void test4() {
+		Assert.assertEquals(true, canPlaceFlowers(new int[] { 1, 0, 0, 1, 0, 0 }, 1));
+	}
+
+}
