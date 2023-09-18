3d2378a4d2e6bb90b9bc23437e1f6fc5002e520a
SivaG <47685173+sivagtesting@users.noreply.github.com>
2023-09-18 21:05:52 +0530

    DailyExercise - 18-Sep-2023

---
+++ src/main/java/mandatoryHomework/week10/day3/CountGoodString_1876.java
@@ -0,0 +1,93 @@
+package mandatoryHomework.week10.day3;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+public class CountGoodString_1876 {
+
+	public int countGoodSubstrings(String s) {
+		if (s.length() < 3)
+			return 0;
+		else {
+			int counter = 0, pOne = 0, pTwo = 3;
+			while (pTwo <= s.length()) {
+				if(pOne==s.length()-3) {
+					if (isAllCharsAreUnique(s.substring(pOne)))
+						counter++;
+					pOne++;
+					pTwo++;
+				} else {
+				if (isAllCharsAreUnique(s.substring(pOne, pTwo)))
+					counter++;
+				pOne++;
+				pTwo++;
+			}
+			}
+			return counter;
+		}
+
+	}
+
+	public boolean isAllCharsAreUnique(String inp) {
+		Set<Character> charSet = new HashSet<Character>();
+		for (char each : inp.toCharArray()) {
+			if (charSet.add(each))
+				continue;
+			else
+				return false;
+		}
+		return charSet.size() == 3;
+
+	}
+	
+	@Test
+	public void test1() {
+		String s = "xyzzaz";
+		int output = 1;
+		Assert.assertEquals(output, countGoodSubstrings(s));
+	}
+	
+	//aababcabc
+	
+	@Test
+	public void test2() {
+		String s = "aababcabc";
+		int output = 4;
+		Assert.assertEquals(output, countGoodSubstrings(s));
+	}
+	
+	/*
+	 * 
+	 * TestData Enumeration:
+"a" -> 0
+"abc" -> 1
+"aaa" -> 0
+"abcedfghijklmno" -> 
+
+PseudoCode: 
+5.01
+if size of input string s is less than 3 then return 0 else do the following
+
+intialize two integers pOne=0 and pTwo =2, counter=0;
+initialize a stringbuilder sb of size 3;
+perform the following using while untile pTwo<s.length();
+get the substring of s with starting and ending index as pOne and pTwo;
+append it to the sb
+send the sb to a method that return true if all char's are unique
+if yes then increment counter
+
+return counter;
+
+method to find all char are uniuque in substring()
+using for each add each char to the Character set
+return set.size()==3;
+5.07
+5.08 dry run: 
+
+Code:
+	 */
+
+}
