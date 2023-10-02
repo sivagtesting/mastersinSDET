8863daa3e98d22c25bbbf0717323b6e8e9a5c088
SivaG <47685173+sivagtesting@users.noreply.github.com>
2023-10-03 02:02:25 +0530

    NumberOfLinesToWriteAString_LC_806

--- src/main/java/foundation/DailyProblems/PixelOfEachChar_806.java
+++ src/main/java/foundation/DailyProblems/PixelOfEachChar_806.java
@@ -5,6 +5,36 @@ import org.testng.annotations.Test;
 
 public class PixelOfEachChar_806 {
 	
+	/*
+	 * https://leetcode.com/problems/number-of-lines-to-write-string/description/
+	 * 
+	 	PseudoCode:
+			11.15
+			intialize a integer pixel=0, counter=0;
+			iterate the following steps using for untile s.length
+			convert the char at current index ascciValue a=0, b=1 and so on
+			check if pixel>100
+			check if i = s.length-1
+			if yes, then add result[1] = pixel;
+			if yes then decrement i and negate the pixel value of charAt current index from pixel integer and increment counter & reset piexl
+			if no then add pixel value of current index to pixel
+			check if piexel<100
+			pixel = pixel + widths[s.charAt(i)-'a'];
+			check if pixel ==100
+			if yes increment counter;
+			check if i = s.length-1
+			if yes, result[1] = pixel;
+			pixel =0;
+			i = i-1
+			
+			result[0] = counter;
+			
+			return result;
+			
+			11.29
+	 * 
+	 */
+	
 	public static int[] numberOfLines(int[] widths, String s) {
 		int pixel = 0, counter = 0;
 		int[] result = new int[2];
