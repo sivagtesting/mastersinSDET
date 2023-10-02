804d8c74b218d2a7ebb320ca3e2f65521862b7fd
SivaG <47685173+sivagtesting@users.noreply.github.com>
2023-10-02 19:24:31 +0530

    AssignCookies_LC_455

--- src/main/java/mandatoryHomework/AfterDSA/AssignCookies_455.java
+++ src/main/java/mandatoryHomework/AfterDSA/AssignCookies_455.java
@@ -1,5 +1,51 @@
 package mandatoryHomework.AfterDSA;
 
+import java.util.Arrays;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class AssignCookies_455 {
+	
+	/*
+	 * https://leetcode.com/problems/assign-cookies/description/
+	 * 
+	 * 
+	 * 
+	 */
+
+	public int findContentChildren(int[] g, int[] s) {
+		Arrays.sort(g);
+		Arrays.sort(s);
+		int start = 0, end = 0, output = 0;
+		while (start < g.length && end < s.length) {
+			// if(start==g.length || end==s.length)
+			// break;
+			if (g[start] <= s[end]) {
+				output++;
+				start++;
+				end++;
+			} else
+				end++;
+
+		}
+		return output;
+	}
+	
+	@Test
+	public void test1() {
+		int[] greed = {1,2,3};
+		int[] cookie = {1,1};
+		int output = 1;
+		Assert.assertEquals(findContentChildren(greed, cookie), output);
+	}
+	
+	@Test
+	public void test2() {
+		int[] greed = {1,2};
+		int[] cookie = {1,2,3};
+		int output = 2;
+		Assert.assertEquals(findContentChildren(greed, cookie), output);
+	}
 
 }
